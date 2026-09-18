3a1d0a56229c28edbd661efb60d7b4a7bf382fb7
sinchancodes28 <sinchanjain300@gmail.com>
2026-09-18 12:32:37 +0530

    Update BalancedBinaryTree.java

--- BalancedBinaryTree.java
+++ BalancedBinaryTree.java
@@ -1,22 +1,31 @@
+
+ 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */ 
+ //for a balanced tree the height of the left subtree and right subtree is <= 1
 class Solution {
-    public List<String> binaryTreePaths(TreeNode root) {
-        List<String> ans = new ArrayList<>();
-        dfs(root, "", ans);
-        return ans;
+    public boolean isBalanced(TreeNode root) {
+        if(root == null) return true;
+        int left = levels(root.left);
+        int right = levels(root.right);
+        if(Math.abs(left - right) > 1) return false;
+        return isBalanced(root.left) && isBalanced(root.right);
     }
-
-    private void dfs(TreeNode root, String path, List<String> ans) {
-        if (root == null) {
-            return;
-        }
-
-        path = path + root.val;
-        if (root.left == null && root.right == null) {
-            ans.add(path);
-            return;
-        }
-
-        dfs(root.left, path + "->", ans);
-        dfs(root.right, path + "->", ans);
+    int levels(TreeNode root){
+       if(root == null )return 0;
+        return 1 + ( Math.max(levels(root.left) , levels(root.right)));
     }
 }
