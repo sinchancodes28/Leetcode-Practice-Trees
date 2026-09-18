4b2d157ca58bf75e7c920a94c606dc8b850c4c58
sinchancodes28 <sinchanjain300@gmail.com>
2026-09-18 11:35:02 +0530

    Add Balanced Binary Tree

---
+++ BalancedBinaryTree.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> ans = new ArrayList<>();
+        dfs(root, "", ans);
+        return ans;
+    }
+
+    private void dfs(TreeNode root, String path, List<String> ans) {
+        if (root == null) {
+            return;
+        }
+
+        path = path + root.val;
+        if (root.left == null && root.right == null) {
+            ans.add(path);
+            return;
+        }
+
+        dfs(root.left, path + "->", ans);
+        dfs(root.right, path + "->", ans);
+    }
+}
